9598a65667c4e48f25ac6738c6f8035af8557bd2
rakesh.bannigol <rakesh.banni94499@gmail.com>
2023-10-06 22:17:13 +0530

    initial commit

---
+++ auth/auth/info.py
@@ -0,0 +1,7 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'rakeshbannigol94499@gmail.com'
+EMAIL_HOST_PASSWORD = "npdf uquh vqbu kooa"
+EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_SSL = False  # Set to True if using SSL
---
+++ auth/authentication/views.py
@@ -0,0 +1,144 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.core.mail import EmailMessage, send_mail
+from auth import settings
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_str
+from django.contrib.auth import authenticate, login, logout
+from . tokens import generate_token
+import urllib.request
+import json
+
+# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        res = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q='+city+
+                                     '&appid=ad8a600eae0614b391b62745648429a3').read()
+        jason_data = json.loads(res)
+        data = {
+            'country_code':str(jason_data['sys']['country']),
+            'coordinate': str(jason_data['coord']['lon'])+"\n"+str(jason_data['coord']['lat']),
+            'temp': str(jason_data['main']['temp']),
+            'pressure':str(jason_data['main']['pressure']),
+            'humidity':str(jason_data['main']['humidity'])
+        }
+    else:
+        city = ''
+        data = {}
+    return render(request, 'index.html', {'city':city, 'data':data})
+
+def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exist! Please try some other username.")
+            return redirect('home')
+        
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email Already Registered!!")
+            return redirect('home')
+        
+        if len(username)>20:
+            messages.error(request, "Username must be under 20 charcters!!")
+            return redirect('home')
+        
+        if pass1 != pass2:
+            messages.error(request, "Passwords didn't matched!!")
+            return redirect('home')
+        
+        if not username.isalnum():
+            messages.error(request, "Username must be Alpha-Numeric!!")
+            return redirect('home')
+        
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        # myuser.is_active = False
+        myuser.is_active = False
+        myuser.save()
+        messages.success(request, "Your Account has been created succesfully!! Please check your email to confirm your email address in order to activate your account.")
+        
+        # Welcome Email
+        subject = "Welcome to GFG- Django Login!!"
+        message = "Hello " + myuser.first_name + "!! \n" + "Welcome to GFG!! \nThank you for visiting our website\n. We have also sent you a confirmation email, please confirm your email address. \n\nThanking You\nAnubhav Madhav"        
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
+        
+        # Email Address Confirmation Email
+        current_site = get_current_site(request)
+        email_subject = "Confirm your Email @ GFG - Django Login!!"
+        message2 = render_to_string('email_confirmation.html',{
+            
+            'name': myuser.first_name,
+            'domain': current_site.domain,
+            'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
+            'token': generate_token.make_token(myuser)
+        })
+        email = EmailMessage(
+        email_subject,
+        message2,
+        settings.EMAIL_HOST_USER,
+        [myuser.email],
+        )
+        email.fail_silently = True
+        email.send()
+        
+        return redirect('signin')
+        
+        
+    return render(request, "signup.html")
+
+
+def activate(request,uid64,token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uid64))
+        myuser = User.objects.get(pk=uid)
+    except (TypeError,ValueError,OverflowError,User.DoesNotExist):
+        myuser = None
+
+    if myuser is not None and generate_token.check_token(myuser,token):
+        myuser.is_active = True
+        # user.profile.signup_confirmation = True
+        myuser.save()
+        login(request,myuser)
+        messages.success(request, "Your Account has been activated!!")
+        return redirect('signin')
+    else:
+        return render(request,'actiavation_failed.html')
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass')
+        
+        user = authenticate(username=username, password=pass1)
+        
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            # messages.success(request, "Logged In Sucessfully!!")
+            return render(request, "index.html",{"fname":fname})
+        else:
+            messages.error(request, "Bad Credentials!!")
+            return redirect('home')
+    
+    return render(request, "signin.html")
+
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged Out Successfully!!")
+    return redirect('home')
